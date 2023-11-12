61a8087eb3ed7f4c4c26a7d3375ef2cb99fbfa7d
victorzllDev <joaovictor12200capa@gmail.com>
2023-11-12 15:33:17 -0300

    New Desinger

---
+++ src/services/path.js
@@ -0,0 +1,8 @@
+// PATH POSTES
+import poster1 from '/src/assets/postes/poster-1.jpg'
+import poster2 from '/src/assets/postes/poster-2.jpg'
+import poster3 from '/src/assets/postes/poster-3.jpg'
+import poster4 from '/src/assets/postes/poster-4.jpeg'
+import poster5 from '/src/assets/postes/poster-5.jpg'
+
+export const posters = [poster1, poster2, poster3, poster4, poster5]
